8f92c97ffdc9e94b71da7b745649a83df28645be
dfgh2727 <dfgh2727@naver.com>
2025-03-25 14:35:54 +0900

    이수환 : GM, Actor, Pawn 추가, 블루프린트 생성

---
+++ UTetrisProject/Source/UTetrisProject/BlockPawn.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BlockPawn.h"
+
+// Sets default values
+ABlockPawn::ABlockPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABlockPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABlockPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABlockPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ UTetrisProject/Source/UTetrisProject/TetrisGM.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "TetrisGM.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UTETRISPROJECT_API ATetrisGM : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
